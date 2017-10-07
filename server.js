b48fab65fb461575e37c371a34cc1e78666e1385
marku_000 <marku_000@AlPadrone>
2017-10-07 22:40:44 +0200

    Neuer Test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
 
+Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
